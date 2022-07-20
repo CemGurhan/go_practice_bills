49c8567442479b0e32a74f1f18f8a2f65620d8c1
CemGurhan <cemg@live.co.uk>
2022-07-20 22:55:19 +0100

    pointers

--- main.go
+++ main.go
@@ -2,30 +2,24 @@ package main
 
 import "fmt"
 
-func updateName(x string) string {
-	x = "wedge"
-	return x
-}
+func updateName(x *string) {
+	*x = "wedge"
 
-func updateMenu(y map[string]float64) {
-	y["coffee"] = 2.99
 }
 
 func main() {
 
 	name := "tifa"
 
-	name = updateName(name)
+	// updateName(name)
 
+	// fmt.Println("memory address of name is: ", &name)
+
+	m := &name
+	// fmt.Println("memory address:", m)
+	// fmt.Println("value at memory address:", *m)
 	fmt.Println(name)
-
-	menu := map[string]float64{
-		"pie":       5.95,
-		"ice cream": 3.99,
-	}
-
-	updateMenu(menu)
-
-	fmt.Println(menu)
+	updateName(m)
+	fmt.Println(name)
 
 }
