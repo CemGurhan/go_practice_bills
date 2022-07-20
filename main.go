6e6b8a91a89ca7b7635f9eb8444623473cc0e658
CemGurhan <cemg@live.co.uk>
2022-07-20 22:11:03 +0100

    pass by value

--- main.go
+++ main.go
@@ -2,42 +2,30 @@ package main
 
 import "fmt"
 
+func updateName(x string) string {
+	x = "wedge"
+	return x
+}
+
+func updateMenu(y map[string]float64) {
+	y["coffee"] = 2.99
+}
+
 func main() {
 
+	name := "tifa"
+
+	name = updateName(name)
+
+	fmt.Println(name)
+
 	menu := map[string]float64{
-		"soup":          4.99,
-		"pie":           7.99,
-		"salad":         6.99,
-		"tofee pudding": 3.55,
+		"pie":       5.95,
+		"ice cream": 3.99,
 	}
+
+	updateMenu(menu)
 
 	fmt.Println(menu)
-	fmt.Println(menu["pie"])
-
-	// looping maps
-
-	for k, v := range menu {
-
-		fmt.Println(k, "-", v)
-
-	}
-
-	// ints as key type
-
-	phonebook := map[int]string{
-
-		26667: "mario",
-		34589: "luigi",
-		89992: "peach",
-	}
-
-	fmt.Println(phonebook)
-	fmt.Println(phonebook[26667])
-
-	phonebook[34589] = "bowser"
-	fmt.Println(phonebook)
-
-	phonebook[89992] = "yoshi"
-	fmt.Println(phonebook)
 
 }
