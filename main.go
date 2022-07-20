8f9df380871a4138e8fd64704fa2a0583a7caa43
CemGurhan <cemg@live.co.uk>
2022-07-20 18:09:48 +0100

    booleans and conditionals

--- main.go
+++ main.go
@@ -6,39 +6,37 @@ import (
 
 func main() {
 
-	// x := 0
-	// for x < 5 {
-	// 	fmt.Println("value of x is:", x)
-	// 	x++
-	// }
+	age := 45
 
-	// for i := 0; i < 5; i++ {
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 45)
+	fmt.Println(age != 50)
 
-	// 	fmt.Println("value of i is:", i)
+	if age < 30 {
+		fmt.Println("age is less than 30")
 
-	// }
-
-	names := []string{"mario", "luigi", "yoshi", "peach"}
-
-	// for i := 0; i < len(names); i++ {
-
-	// 	fmt.Println(names[i])
-
-	// }
-
-	// for i, v := range names {
-
-	// 	fmt.Printf("the value at index %v is %v \n", i, v)
-
-	// }
-
-	for _, v := range names {
-
-		fmt.Printf("the value is %v \n", v)
-		v = "new string"
-
+	} else if age < 40 {
+		fmt.Println("age is less than 40")
+	} else {
+		fmt.Println("age is not less than 45")
 	}
 
-	fmt.Println(names)
+	names := []string{"mario", "luigi", "yoshi", "peach", "bowser"}
+
+	for index, value := range names {
+
+		if index == 1 {
+			fmt.Println("continuing at pos ", index)
+			continue
+		}
+		if index > 2 {
+			fmt.Println("breaking at pos", index)
+			break
+		}
+
+		fmt.Printf("the value at position %v is %v \n", index, value)
+
+	}
 
 }
