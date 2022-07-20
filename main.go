5dedb25cb005357616296abdac71b4009e3594aa
CemGurhan <cemg@live.co.uk>
2022-07-20 16:12:18 +0100

    strings and int declaration praxtice

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//strings
+
+	var nameOne string = "stringy"
+	var nameTwo = "lui"
+	var nameThree string
+
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameOne = "peach"
+	nameThree = "bowser"
+
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameFour := "yoshi"
+
+	fmt.Println(nameFour)
+
+	//ints
+
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+}
