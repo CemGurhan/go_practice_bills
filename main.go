31058d93ea60ee2557f49015d92b370f2a06eca7
CemGurhan <cemg@live.co.uk>
2022-07-21 12:14:30 +0100

    recievers

--- main.go
+++ main.go
@@ -2,24 +2,10 @@ package main
 
 import "fmt"
 
-func updateName(x *string) {
-	*x = "wedge"
-
-}
-
 func main() {
 
-	name := "tifa"
+	myBill := newBill("marios bill")
 
-	// updateName(name)
-
-	// fmt.Println("memory address of name is: ", &name)
-
-	m := &name
-	// fmt.Println("memory address:", m)
-	// fmt.Println("value at memory address:", *m)
-	fmt.Println(name)
-	updateName(m)
-	fmt.Println(name)
+	fmt.Println(myBill.format())
 
 }
